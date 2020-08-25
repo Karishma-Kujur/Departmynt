d6bde72a55179dc900889d9deefae4d605b51fc3
karishma-kujur <karishm@softsuave.com>
2020-08-25 23:11:07 +0530

    password validation

--- src/schema/signup.js
+++ src/schema/signup.js
@@ -9,6 +9,10 @@ export const validationSchema = Yup.object().shape({
         .required("*Please Enter your email Id")
         .email("*Please Enter a valid email Id"),
     password: Yup.string()
+        .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        )
         .required("*Please Enter your password"),
     confirmPassword: Yup.string()
         .required("*Please Re Enter your Password")
